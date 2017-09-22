d9e7cf8041f335d2f782eaef6c0495bbf3522b6f
Jason Oswald <joswald@roddy203teachm.baylorschool.org>
2017-09-22 12:47:06 -0400

    Initial Commit

---
+++ Sudoku2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sudoku2017
+//
+//  Created by Jason Oswald on 9/22/17.
+//  Copyright © 2017 CS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
